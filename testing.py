33909b20b3006f2ee45d7171811d9f4331b21c60
aghee <agymunzala@gmail.com>
2023-07-26 13:24:28 +0300

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,17 @@
+def ttt(val):
+    print('\n')
+    print('\t  |  |')
+    print('\t {} | {} |{}'.format(val[0],val[1],val[2]))
+    print('\t ______|______|________')
+    
+    print('\t  |  |')
+    print('\t {} | {} |{}'.format(val[3],val[4],val[5]))
+    print('\t ______|______|________')
+
+    print('\t  |  |')
+
+    print('\t {} | {} |{}'.format(val[3],val[4],val[5]))
+    print('\t  |  |')
+    print('\n')
+
+ttt()
